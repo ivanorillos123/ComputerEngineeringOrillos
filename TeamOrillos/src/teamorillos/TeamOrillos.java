0b71dc20cdc40e0760bb4a0ccce8940e73d7094c
WS15 <WS15@WS15>
2016-02-02 08:42:39 +0800

    Yow!

---
+++ TeamOrillos/src/teamorillos/TeamOrillos.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teamorillos;
+
+/**
+ *
+ * @author WS15
+ */
+public class TeamOrillos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("Computer Engineering");
+    }
+    
+}
